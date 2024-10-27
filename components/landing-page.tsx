a059b2e997341fbfa00e139a88a817519a7981f9
hitikadwani <hitikadwani0902@gmail.com>
2024-10-27 19:24:49 +0530

    db_seeding

--- components/landing-page.tsx
+++ components/landing-page.tsx
@@ -130,7 +130,7 @@ export function LandingPage() {
         </section>
       </main>
       <footer className="flex flex-col gap-2 sm:flex-row py-6 w-full shrink-0 items-center px-4 md:px-6 border-t border-gray-800 bg-gray-900">
-        <p className="text-xs text-gray-400">© 2024 MusicStream. All rights reserved.</p>
+        <p className="text-xs text-gray-400">© 2024 Muzy. All rights reserved.</p>
         <nav className="sm:ml-auto flex gap-4 sm:gap-6">
           <Link className="text-xs hover:text-purple-400 transition-colors" href="#">
             Terms of Service
