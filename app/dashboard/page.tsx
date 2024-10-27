b5588d16e021be0197ab6865397b5a8157ffb6cd
hitikadwani <hitikadwani0902@gmail.com>
2024-10-27 19:30:39 +0530

    db_seeding_again

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -4,7 +4,7 @@ import 'react-toastify/dist/ReactToastify.css'
 import StreamView from '../components/StreamView'
 
 
-const creatorId = "704cffdb-e164-4847-832e-7169ecda3638"
+const creatorId = "cc64dfc0-36f2-42e3-bad7-b0b40e0b9ab6"
 export default function Component() {
     return <StreamView creatorId={creatorId} playVideo={true} />
 }
